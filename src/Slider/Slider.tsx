a696b06240916503e1f29a101a3056866b66d0f1
Bartosz Nocun <bartosznocun1@gmail.com>
2022-05-01 22:18:33 +0200

    feat(Slider): set arrows disabled for both ends of slider

--- src/Slider/Slider.tsx
+++ src/Slider/Slider.tsx
@@ -1,4 +1,4 @@
-import { FC, ReactNode, useRef } from 'react'
+import { FC, ReactNode, useRef, useState } from 'react'
 import styled from 'styled-components'
 import Container from '../Container'
 import { mq } from '../../styles.config'
@@ -79,18 +79,22 @@ const Header = styled.div`
       transform: translate(2px, 2px);
     }
 
-    &:hover {
+    &:hover:enabled {
       transform: translate(-2px, -2px);
       &:before {
         transform: translate(4px, 4px);
       }
     }
 
-    &:active {
+    &:active:enabled {
       transform: translate(0px, 0px);
       &:before {
         transform: translate(2px, 2px);
       }
+    }
+
+    &:disabled {
+      background-color: gray;
     }
   }
 `
@@ -103,6 +107,36 @@ const Controls = styled.div`
 const Slider: FC<{ children: ReactNode[]; title: string }> = ({ children, title }) => {
   const sliderRef = useRef<null | HTMLDivElement>(null)
   const trackRef = useRef<null | HTMLDivElement>(null)
+
+  const [isFirstVisible, setIsFirstVisible] = useState<boolean>(true)
+  const [isLastVisible, setIsLastVisible] = useState<boolean>(false)
+
+  const scrollHandler = (sliderElem: HTMLDivElement | null, dest: number = 0) => {
+    if (!sliderElem) return
+    const firstSlide = sliderElem.firstChild?.firstChild as Element
+    const lastSlide = sliderElem.firstChild?.lastChild as Element
+    if (!firstSlide) return
+
+    if (firstSlide.getBoundingClientRect().left < 0 + dest) {
+      setIsFirstVisible(false)
+    } else {
+      setIsFirstVisible(true)
+    }
+
+    if (lastSlide.getBoundingClientRect().right > window.innerWidth + dest) {
+      setIsLastVisible(false)
+    } else {
+      setIsLastVisible(true)
+    }
+  }
+
+  const handleTouchEnd = () => {
+    const delay = 500
+
+    setTimeout(() => {
+      scrollHandler(sliderRef.current)
+    }, delay)
+  }
 
   const scrollSlides = (slidesToScroll: number) => {
     const sliderElem = sliderRef.current
@@ -117,6 +151,8 @@ const Slider: FC<{ children: ReactNode[]; title: string }> = ({ children, title 
       left: destination,
       behavior: 'smooth'
     })
+
+    scrollHandler(sliderElem, destination)
   }
 
   return (
@@ -125,7 +161,7 @@ const Slider: FC<{ children: ReactNode[]; title: string }> = ({ children, title 
         <Header>
           <h2>{title}</h2>
           <Controls>
-            <button onClick={() => scrollSlides(-2)}>
+            <button onClick={() => scrollSlides(-2)} disabled={isFirstVisible}>
               <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path
                   d="M6.85355 3.14645C7.04882 3.34171 7.04882 3.65829 6.85355 3.85355L3.70711 7H12.5C12.7761 7 13 7.22386 13 7.5C13 7.77614 12.7761 8 12.5 8H3.70711L6.85355 11.1464C7.04882 11.3417 7.04882 11.6583 6.85355 11.8536C6.65829 12.0488 6.34171 12.0488 6.14645 11.8536L2.14645 7.85355C1.95118 7.65829 1.95118 7.34171 2.14645 7.14645L6.14645 3.14645C6.34171 2.95118 6.65829 2.95118 6.85355 3.14645Z"
@@ -133,7 +169,7 @@ const Slider: FC<{ children: ReactNode[]; title: string }> = ({ children, title 
                 />
               </svg>
             </button>
-            <button onClick={() => scrollSlides(2)}>
+            <button onClick={() => scrollSlides(2)} disabled={isLastVisible}>
               <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path
                   d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z"
@@ -145,7 +181,7 @@ const Slider: FC<{ children: ReactNode[]; title: string }> = ({ children, title 
         </Header>
       </Container>
       <StrictSlider ref={sliderRef}>
-        <Track ref={trackRef} numberOfSlides={children?.length}>
+        <Track ref={trackRef} numberOfSlides={children?.length} onTouchEnd={handleTouchEnd}>
           {children}
         </Track>
       </StrictSlider>
