f3e9117c7667ef2f676c7ec62b0f11ed91142579
OktawiaRogowicz <oktawia.rogowicz.30@gmail.com>
2022-05-31 20:19:18 +0200

    Added button:disabled with intersectionObserver

--- src/Slider/Slider.tsx
+++ src/Slider/Slider.tsx
@@ -1,7 +1,8 @@
-import { FC, ReactNode, useRef } from 'react'
+import { FC, ReactNode, useEffect, useRef } from 'react'
 import styled from 'styled-components'
 import Container from '../Container'
 import { mq } from '../../styles.config'
+import { useState } from 'react'
 
 const Root = styled.section`
   position: relative;
@@ -33,7 +34,7 @@ const Track = styled.div<{ numberOfSlides: number }>`
   grid-gap: 20px;
   grid-auto-flow: column;
   // grid-auto-columns: minmax(70vw, 400px);
-  grid-auto-columns: 280px;
+  grid-auto-columns: 280px 280px;
   & > * {
     scroll-snap-align: center;
   }
@@ -48,13 +49,18 @@ const Track = styled.div<{ numberOfSlides: number }>`
 `
 const Header = styled.div`
   display: flex;
+  
   justify-content: space-between;
   align-items: center;
   margin-bottom: 40px;
 
   h2 {
     margin: 0;
-    font-size: 40px;
+    font-size: 20px;
+
+    ${mq['large']} {
+      font-size: 40px;
+    }
   }
 
   button {
@@ -92,6 +98,16 @@ const Header = styled.div`
         transform: translate(2px, 2px);
       }
     }
+
+    &:disabled, &[disabled]{
+      cursor: not-allowed;
+      pointer-events: none;
+      opacity: 0.5;
+      transform: translate(0px, 0px);
+      &:before {
+        transform: translate(2px, 2px);
+      }
+    }
   }
 `
 const Controls = styled.div`
@@ -100,15 +116,29 @@ const Controls = styled.div`
   grid-template-columns: 44px 44px;
   grid-template-rows: 44px;
 `
+
+function isSlideIntersecting(entry: IntersectionObserverEntry, slide: Element, buttonDisable: Function) {
+  if(entry.target === slide) {
+    buttonDisable(entry.isIntersecting)
+  }
+}
+
 const Slider: FC<{ children: ReactNode[]; title: string }> = ({ children, title }) => {
   const sliderRef = useRef<null | HTMLDivElement>(null)
   const trackRef = useRef<null | HTMLDivElement>(null)
+  const observerRef = useRef<null | IntersectionObserver>(null)
+
+  const [isFirstSlideVisible, setIsFirstSlideVisible] = useState(true);
+  const [isLastSlideVisible, setIsLastSlideVisible] = useState(false);
 
   const scrollSlides = (slidesToScroll: number) => {
     const sliderElem = sliderRef.current
     if (!sliderElem) return
-    const slide1 = sliderElem.firstChild?.childNodes[0] as Element
-    const slide2 = sliderElem.firstChild?.childNodes[1] as Element
+
+    const slides = sliderElem.firstChild?.childNodes as NodeListOf<Element>;
+    const slide1 = slides[0];
+    const slide2 = slides[1];
+
     if (!slide1 || !slide2) return
     const slideWidth = slide1.getBoundingClientRect().width
     const gutterWidth = slide2.getBoundingClientRect().left - slide1.getBoundingClientRect().right
@@ -119,13 +149,32 @@ const Slider: FC<{ children: ReactNode[]; title: string }> = ({ children, title 
     })
   }
 
+  useEffect(() => {
+    const firstSlideRef = trackRef.current?.firstChild as Element;
+    const lastSlideRef = trackRef.current?.lastChild as Element;
+
+    if (observerRef.current) observerRef.current.disconnect() 
+
+    observerRef.current = new IntersectionObserver(entries => { 
+      entries.forEach(entry => {
+        isSlideIntersecting(entry, firstSlideRef, setIsFirstSlideVisible);
+        isSlideIntersecting(entry, lastSlideRef, setIsLastSlideVisible);
+      });
+    }, {
+      threshold: 1
+    })
+
+    observerRef.current?.observe(firstSlideRef);
+    observerRef.current?.observe(lastSlideRef);
+  })
+
   return (
     <Root>
       <Container>
         <Header>
           <h2>{title}</h2>
           <Controls>
-            <button onClick={() => scrollSlides(-2)}>
+            <button disabled={isFirstSlideVisible} onClick={() => scrollSlides(-2)}>
               <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path
                   d="M6.85355 3.14645C7.04882 3.34171 7.04882 3.65829 6.85355 3.85355L3.70711 7H12.5C12.7761 7 13 7.22386 13 7.5C13 7.77614 12.7761 8 12.5 8H3.70711L6.85355 11.1464C7.04882 11.3417 7.04882 11.6583 6.85355 11.8536C6.65829 12.0488 6.34171 12.0488 6.14645 11.8536L2.14645 7.85355C1.95118 7.65829 1.95118 7.34171 2.14645 7.14645L6.14645 3.14645C6.34171 2.95118 6.65829 2.95118 6.85355 3.14645Z"
@@ -133,7 +182,7 @@ const Slider: FC<{ children: ReactNode[]; title: string }> = ({ children, title 
                 />
               </svg>
             </button>
-            <button onClick={() => scrollSlides(2)}>
+            <button disabled={isLastSlideVisible} onClick={() => scrollSlides(2)}>
               <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path
                   d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z"
