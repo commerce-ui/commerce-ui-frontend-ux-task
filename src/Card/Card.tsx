11061478b477f9b19d53c8fe4ad94ba79439490e
m4tt133 <kapalamateusz81@gmail.com>
2023-06-02 16:03:06 +0200

    Fix - Intersection Observer | Play Video on 0.75 Threshold

--- src/Card/Card.tsx
+++ src/Card/Card.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react'
+import { FC, useEffect, useRef } from 'react'
 import { CardProps } from './types'
 import Image from 'next/image'
 import styled from 'styled-components'
@@ -26,15 +26,36 @@ const ContentWrap = styled.div`
 `
 
 const Card: FC<CardProps> = ({ title, href, media }) => {
+
+  const ref = useRef<HTMLVideoElement | null>(null);
+
+  useEffect(() => {
+    if (!ref.current) return;
+
+    const videoRef = ref.current;
+    const observer = new IntersectionObserver(([entry]) => {
+      
+      if(entry.isIntersecting){
+        ref.current?.play()
+      }else{
+        ref.current?.pause()
+      }
+    }, { threshold: 0.75 });
+
+    observer.observe(videoRef);
+
+    return () => observer.unobserve(videoRef);
+  }, [ref])
+  
   return (
     <Root>
       <ImageWrap>
         {media.resource_type === 'image' && <Image {...media} />}
 
         {media.resource_type === 'video' && (
-          <video autoPlay muted loop playsInline>
-            <source type={'video/' + media.format} src={media.src} />
-          </video>
+            <video ref={ref} muted loop playsInline >
+              <source type={'video/' + media.format} src={media.src} />
+            </video>
         )}
       </ImageWrap>
       <ContentWrap>
